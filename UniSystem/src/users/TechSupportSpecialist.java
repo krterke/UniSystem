c19dcf5d2f3da0a031ae37de209cefa73367bb43
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-15 16:14:02 +0600

    Update TechSupportSpecialist.java

--- UniSystem/src/users/TechSupportSpecialist.java
+++ UniSystem/src/users/TechSupportSpecialist.java
@@ -1,25 +1,25 @@
-package users;
+package Users;
 
+import java.io.Serializable;
 
-/**
-* @generated
-*/
-public class TechSupportSpecialist extends Employee {
-    
-    
-    
+import Enums.Gender;
 
+public class TechSupportSpecialist extends Employee implements Serializable{
+	
+	public TechSupportSpecialist() {}
     
-    
-
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
+    public TechSupportSpecialist(String name, String surname, Gender gender, String email) throws Exception {
+    	super(name, surname, gender, email);
+    }
+   
     public void acceptOrder() {
         //TODO
     }
     
     
+    public static TechSupportSpecialist createTechSupportSpecialist(String name, String surname, Gender gender, String email) throws Exception {
+    	return new TechSupportSpecialist(name, surname, gender, email);
+    }
+    
 }
+
