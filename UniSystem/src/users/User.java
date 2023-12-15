72327611cfcba222c7762ed1aaf172ab910527b3
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-15 16:13:23 +0600

    Update User.java

--- UniSystem/src/users/User.java
+++ UniSystem/src/users/User.java
@@ -1,266 +1,110 @@
-package users;
+package Users;
 
+import Additional.*;
+import Enums.*;
 
-/**
-* @generated
-*/
 public class User {
     
-    /**
-    * @generated
-    */
+
     private String name;
-    
-    /**
-    * @generated
-    */
     private String surname;
-    
-    /**
-    * @generated
-    */
     private Gender gender;
-    
-    /**
-    * @generated
-    */
     private String id;
-    
-    /**
-    * @generated
-    */
     private Login login;
-    
-    /**
-    * @generated
-    */
     private String email;
-    
-    /**
-    * @generated
-    */
-    private int phoneNumber;
-    
-    
-    /**
-    * @generated
-    */
     private Admin admin;
-    
-    /**
-    * @generated
-    */
     private System system;
     
-    /**
-    * @generated
-    */
-    private Login login2;
+    public User () {};
     
+    public User (String name, String surname, Gender gender, String email) {
+    	//this.login = login;
+    	this.name = name;
+     	this.surname = surname;
+     	this.gender = gender;
+     	this.email = email;
+    };
     
-
-    /**
-    * @generated
-    */
     private String getName() {
         return this.name;
     }
     
-    /**
-    * @generated
-    */
-    private String setName(String name) {
+    private void setName(String name) {
         this.name = name;
     }
-    
-    
-    /**
-    * @generated
-    */
+
     private String getSurname() {
         return this.surname;
     }
     
-    /**
-    * @generated
-    */
-    private String setSurname(String surname) {
+    private void setSurname(String surname) {
         this.surname = surname;
     }
     
-    
-    /**
-    * @generated
-    */
     private Gender getGender() {
         return this.gender;
     }
     
-    /**
-    * @generated
-    */
-    private Gender setGender(Gender gender) {
+    private void setGender(Gender gender) {
         this.gender = gender;
     }
     
-    
-    /**
-    * @generated
-    */
     private String getId() {
         return this.id;
     }
     
-    /**
-    * @generated
-    */
-    private String setId(String id) {
+    private void setId(String id) {
         this.id = id;
     }
     
-    
-    /**
-    * @generated
-    */
     private Login getLogin() {
         return this.login;
     }
     
-    /**
-    * @generated
-    */
-    private Login setLogin(Login login) {
+    private void setLogin(Login login) {
         this.login = login;
     }
     
-    
-    /**
-    * @generated
-    */
     private String getEmail() {
         return this.email;
     }
     
-    /**
-    * @generated
-    */
-    private String setEmail(String email) {
+    private void setEmail(String email) {
         this.email = email;
     }
     
-    
-    /**
-    * @generated
-    */
-    private int getPhoneNumber() {
-        return this.phoneNumber;
-    }
-    
-    /**
-    * @generated
-    */
-    private int setPhoneNumber(Integer phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-    
-    
-    
-    /**
-    * @generated
-    */
-    public Login getLogin2() {
-        return this.login2;
-    }
-    
-    /**
-    * @generated
-    */
-    public Login setLogin2(Login login2) {
-        this.login2 = login2;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public System getSystem() {
-        return this.system;
-    }
-    
-    /**
-    * @generated
-    */
-    public System setSystem(System system) {
-        this.system = system;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public Admin getAdmin() {
-        return this.admin;
-    }
-    
-    /**
-    * @generated
-    */
-    public Admin setAdmin(Admin admin) {
-        this.admin = admin;
-    }
-    
-    
-    
-
-    //                          Operations                                  
-    
-    /**
-    * @generated
-    */
     public int compareTo() {
         //TODO
         return 0;
     }
     
-    /**
-    * @generated
-    */
     public int hashCode() {
         //TODO
         return 0;
     }
     
-    /**
-    * @generated
-    */
     public boolean equals() {
         //TODO
         return false;
     }
     
-    /**
-    * @generated
-    */
     public boolean logIn() {
         //TODO
         return false;
     }
     
-    /**
-    * @generated
-    */
     public boolean logOut() {
         //TODO
         return false;
     }
     
-    /**
-    * @generated
-    */
     public boolean changePassword() {
         //TODO
         return false;
     }
     
+    public String toString() {
+		return "Name: " + name +" Surname: " + surname + " Gender: " + gender + " Email: " + email;
+	}
     
 }
