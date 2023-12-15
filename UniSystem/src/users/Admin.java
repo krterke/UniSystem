98bac14d5bfb03cc21f762e2767d344e64961ac9
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-15 14:55:03 +0600

    Update Admin.java

--- UniSystem/src/users/Admin.java
+++ UniSystem/src/users/Admin.java
@@ -1,58 +1,90 @@
-package users;
+package Users;
 
 
-/**
-* @generated
-*/
-public class Admin extends Employee {
-    
-    
-    /**
-    * @generated
-    */
-    private User user;
-    
-    /**
-    * @generated
-    */
-    private LogFile logFile;
-    
-    
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Serializable;
+import java.util.Vector;
 
-    
-    /**
-    * @generated
-    */
-    public LogFile getLogFile() {
-        return this.logFile;
-    }
-    
-    /**
-    * @generated
-    */
-    public LogFile setLogFile(LogFile logFile) {
-        this.logFile = logFile;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    public User getUser() {
-        return this.user;
-    }
-    
-    /**
-    * @generated
-    */
-    public User setUser(User user) {
-        this.user = user;
-    }
-    
-    
-    
+import Enums.Gender;
+import Additional.*;
+import Database.Database;
 
-    //                          Operations                                  
-    
-    
+public class Admin extends Employee implements Serializable{
+	
+	//fields
+	private static Admin instance;
+	
+	//constructors
+	private Admin() {}
+	private Admin(String name, String surname, Gender gender, String email) throws Exception {
+		super(name, surname, gender, email);
+	}
+	
+	public static Admin getInstance(String name, String surname, Gender gender, String email) throws Exception {
+		if(instance == null) {
+			instance = new Admin(name, surname, gender, email);
+		}
+		return instance;
+	}
+	
+	//methods
+	public void addUser(String name,  String surname, Gender gender, String email) throws NumberFormatException, Exception {
+		//User newUser = new User(email, phoneNumber, id, gender, name, surname);
+		System.out.println("Choose type of user:\n1.Student\n2.Teacher\n3.Manager\n4.TechSupportSpecialist\n5.Researcher\nexit");
+		try(BufferedReader br = new BufferedReader (new InputStreamReader(System.in))) {
+			String text;
+			Database db = Database.getInstance();
+			while(true){
+				text=br.readLine();
+				if(text.equals("exit")) {
+					break;
+				}
+				if(text.equals("Student")) {
+					db.addUser(Student.createStudent(name, surname, gender, email));
+					break;
+				}
+				else if(text.equals("Teacher")) {
+					db.addUser(Teacher.createTeacher(name, surname, gender, email));
+					break;
+				}
+				else if(text.equals("Manager")) {
+					db.addUser(Manager.createManager(name, surname, gender, email));
+					break;
+				}
+				else if(text.equals("TechSupportSpecialist")) {
+					db.addUser(TechSupportSpecialist.createTechSupportSpecialist(name, surname, gender, email));
+					break;
+				}
+//				else if(text.equals("Researcher")) {
+//					db.addUser(Researcher.createResearcher(email, phoneNumber, gender, name, surname));
+//					break;
+//				}
+            }
+		}	
+		catch(IOException ex) {
+			ex.printStackTrace();
+		}
+		
+	}
+	public void deleteUser(int id) {
+		//
+	}
+	public void updateUser() {
+		//
+	}
+	
+	public void seeLogFiles() {
+		//
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+
 }
+
