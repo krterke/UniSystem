6109677c6ad74e4b0ce942b56b308f7aa72a3cc1
Yerkezhan Kurtulush <95469009+krterke@users.noreply.github.com>
2023-12-21 15:34:21 +0600

    Update Time.java

--- UniSystem/src/additional/Time.java
+++ UniSystem/src/additional/Time.java
@@ -1,74 +1,56 @@
 package additional;
 
+import Enums.WeekDays;
+import course.Lesson;
 
-/**
-* @generated
-*/
 public class Time {
-    
-    /**
-    * @generated
-    */
     private WeekDays day;
-    
-    /**
-    * @generated
-    */
-    private int hour;
-    
-    
-    /**
-    * @generated
-    */
+    private Integer hour;
+    private Integer minutes;
     private Lesson lesson;
-    
-    
 
-    /**
-    * @generated
-    */
+    public Time(){
+
+    }
+    public Time(WeekDays day, Integer hour,Integer minutes, Lesson lesson;){
+        this.day = day;
+        this.hour = hour;
+        this.minutes = minutes;
+        this.lesson = lesson;
+    }
+
     public WeekDays getDay() {
         return this.day;
     }
-    
-    /**
-    * @generated
-    */
     public WeekDays setDay(WeekDays day) {
         this.day = day;
     }
-    
-    
-    /**
-    * @generated
-    */
-    public int getHour() {
+    public Integer getHour() {
         return this.hour;
     }
-    
-    /**
-    * @generated
-    */
-    public int setHour(Integer hour) {
+    public Integer setHour(Integer hour) {
         this.hour = hour;
     }
-    
-    
-    
-    /**
-    * @generated
-    */
-    public Lesson getLesson() {
-        return this.lesson;
+    public Integer getMinutes() {
+        return this.minutes;
     }
-    
-    /**
-    * @generated
-    */
-    public Lesson setLesson(Lesson lesson) {
-        this.lesson = lesson;
+    public Integer setMinutes(Integer minutes) {
+        this.minutes = minutes;
     }
-    
-    
+
+    public Integer getTime(Lesson l){
+        for(Lesson cur:lesson){
+            if(cur.equals(l)){
+                return "Time: "+l.getHour()+":"+l.getMinutes();
+            }
+        }
+    }
+
+
+    public String toString() {
+		return "Day: "+day+"\n"+"Lesson: "+lesson+"\n"+"Time: "+hour+":"+minutes;
+	}
+
+
     
 }
