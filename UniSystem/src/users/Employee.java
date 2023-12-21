259db8145ee30cdf6c1aae335a1cf7ba05fcf20c
amina niyazbek <107414625+aminokn@users.noreply.github.com>
2023-12-21 17:05:36 +0600

    Update Employee.java

--- UniSystem/src/users/Employee.java
+++ UniSystem/src/users/Employee.java
@@ -6,35 +6,52 @@ import java.util.Vector;
 import Additional.*;
 import Enums.Gender;
 
-public class Employee extends User {
-
-    private Vector<String> messages;
-    private double salary;
+public class Employee extends User implements Serializable{
     
-	public Employee() {}
-    public Employee(String name, String surname, Gender gender, String email) throws Exception {
-		super(name, surname, gender, email);
+	private double salary;
+	
+	public Employee() {
+		
 	}
-    
-    
-    
-    
-    private Vector<String> getMessages() {
-        return this.messages;
-    }
-    
-    private void setMessages(Vector<String> messages) {
-        this.messages = messages;
-    }
-  
-    public String getMessage() {
-        //TODO
-        return "";
-    }
-    
-    
-    public String toString() {
-        return super.toString();
-    }
-    
+	
+	public Employee(String name,String surname,String email, int phoneNumber,Gender gender,double salary) {
+		super(name,surname,email,phoneNumber,gender);
+		this.salary=salary;
+	}
+	
+	public Employee(String name,String surname,String email, int phoneNumber) {
+		super(name,surname, email, phoneNumber);
+	}
+	public Employee(String name,String surname) {
+		super(name, surname);
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	public void sendMessage(String title,String text,String name,String surname){
+		Employee e=new Employee();
+		for(User cur:Database.users) {
+			if(cur.getName().equals(name) && cur.getSurname().equals(surname)) {
+				e=(Employee)cur;
+			}
+		}
+		SMS s=new SMS(this,e,title,text);
+		Database.messages.add(s);
+	}
+	
+	public String readMessage() {
+		for(SMS s:Database.messages) {
+			if(s.getReader().equals(this)) {
+				return s.toString();
+			}
+		}
+		return "You don't have a message";
+	}
+
 }
