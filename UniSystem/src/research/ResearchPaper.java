198abb2ab3ccde4072e353991495640e0b44c0a5
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-21 17:13:38 +0600

    Update ResearchPaper.java

--- UniSystem/src/research/ResearchPaper.java
+++ UniSystem/src/research/ResearchPaper.java
@@ -1,106 +1,81 @@
 package research;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-/**
-* @generated
-*/
-public class ResearchPaper {
+public class ResearchPaper implements Comparable<ResearchPaper>{
     
-    /**
-    * @generated
-    */
-    private Date date;
-    
-    /**
-    * @generated
-    */
-    private String text;
-    
-    /**
-    * @generated
-    */
-    private String citation;
+		private String title;
+	    private String authors;
+	    private String journal;
+	    private int pages;
+	    private Date publicationDate;
+	    private String doi;
+	    private String citation;
+	    private int citationCount;
     
     
-    /**
-    * @generated
-    */
-    private ResearchProject researcgProject;
-    
-    
+	    public ResearchPaper(String title, String authors, String journal, int pages, Date publicationDate, String doi) {
+	        this.title = title;
+	        this.authors = authors;
+	        this.journal = journal;
+	        this.pages = pages;
+	        this.publicationDate = publicationDate;
+	        this.doi = doi;
+	        this.citation = generateCitation();
+	        this.citationCount = 1; // Start with 1 citation for the original publication
+	    }
+	   
+	    
+	 
+	    public String getTitle() {
+	        return title;
+	    }
 
-    /**
-    * @generated
-    */
-    private Date getDate() {
-        return this.date;
-    }
-    
-    /**
-    * @generated
-    */
-    private Date setDate(Date date) {
-        this.date = date;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    private String getText() {
-        return this.text;
-    }
-    
-    /**
-    * @generated
-    */
-    private String setText(String text) {
-        this.text = text;
-    }
-    
-    
-    /**
-    * @generated
-    */
-    private String getCitation() {
-        return this.citation;
-    }
-    
-    /**
-    * @generated
-    */
-    private String setCitation(String citation) {
-        this.citation = citation;
-    }
-    
-    
-    
-    /**
-    * @generated
-    */
-    public ResearchProject getResearcgProject() {
-        return this.researcgProject;
-    }
-    
-    /**
-    * @generated
-    */
-    public ResearchProject setResearcgProject(ResearchProject researcgProject) {
-        this.researcgProject = researcgProject;
-    }
-    
-    
-    
+	    public String getAuthors() {
+	        return authors;
+	    }
 
-    //                          Operations                                  
+	    public String getJournal() {
+	        return journal;
+	    }
+
+	    public int getPages() {
+	        return pages;
+	    }
+
+	    public Date getPublicationDate() {
+	        return publicationDate;
+	    }
+	    
+	    public String getDoi() {
+	        return doi;
+	    }
+	    
+	    public String getCitation() {
+	        return citation;
+	    }
+
+	    private String generateCitation() {
+	        SimpleDateFormat yearFormat = new SimpleDateFormat("yyyy");
+	        String year = yearFormat.format(publicationDate);
+
+	        return authors + ". (" + year + "). " + title + ". " + journal + ", " + pages + ".";
+	    }
+
     
-    /**
-    * @generated
-    */
-    public int compareTo() {
-        //TODO
-        return 0;
-    }
+	    @Override
+	    public int compareTo(ResearchPaper otherPaper) {
+	        // Логика сравнения, например, по дате публикации
+	        return this.publicationDate.compareTo(otherPaper.getPublicationDate());
+	    }
     
     
+	    public int getCitationCount() {
+	        return citationCount;
+	    }
+
+	    public void incrementCitationCount() {
+	        citationCount++;
+	    }
 }
