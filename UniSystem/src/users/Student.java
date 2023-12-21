96a93f9634a4a602b51e35020bab3e3f2bd319c3
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-21 14:13:00 +0600

    Update Student.java

--- UniSystem/src/users/Student.java
+++ UniSystem/src/users/Student.java
@@ -2,14 +2,16 @@ package Users;
 
 import Enums.*;
 
-import Users.*;
+
+//import Users.*;
 import Additional.*;
 import Package.*;
 import java.io.Serializable;
+import java.util.Vector;
 
-public class Student extends User implements Serializable
+public class Student extends User implements Serializable{
 //implements OptionalResearcher 
-{
+	private static final long serialVersionUID = 123456789L;
     /////
     
     private String studentId;
@@ -20,108 +22,260 @@ public class Student extends User implements Serializable
     private Integer credits;
     private Course course;
     private Organization organization;
+    private Vector<Course> enrolledCourses;
     
     
     public Student() {}
     
     public Student(String name, String surname, Gender gender, String email) {
     	super(name, surname, gender, email);
+    	enrolledCourses = new Vector<>();
     }
     
-    private Faculty getFaculty() {
+    
+    
+    public Faculty getFaculty() {
         return this.faculty;
     }
     
-    private Degree getDegree() {
+    public Degree getDegree() {
         return this.degree;
     }
     
-    private String getStudentId() {
+    public String getStudentId() {
         return this.studentId;
     }
     
-    private void setStudentId(String studentId) {
+    public void setStudentId(String studentId) {
         this.studentId = studentId;
     }
     
-    private Integer getYearOfStudy() {
+    public Integer getYearOfStudy() {
         return this.yearOfStudy;
     }
     
-    private void setYearOfStudy(Integer yearOfStudy) {
+    public void setYearOfStudy(Integer yearOfStudy) {
         this.yearOfStudy = yearOfStudy;
     }
 
     
-    private Double getGPA() {
+    public Double getGPA() {
         return this.GPA;
     }
     
-    private void setGPA(Double GPA) {
+    public void setGPA(Double GPA) {
         this.GPA = GPA;
     }
     
-    private Integer getCredits() {
+    public Integer getCredits() {
         return this.credits;
     }
     
-    private void setCredits(Integer credits) {
+    public void setCredits(Integer credits) {
         this.credits = credits;
     }
     
     
     public String viewCourse() {
-        //TODO
-        return "";
+        if (course != null) {
+            return "Current Course: " + course.getCourseName(); // Assuming Course class has a method to get the course name
+        } else {
+            return "Not enrolled in any course.";
+        }
     }
     
-    public boolean addCourse() {
-        //TODO
-        return false;
-    }
     
-    public boolean dropCourse() {
-        //TODO
-        return false;
+    
+    public void addCourse(Course course) {
+        if (course != null) {
+            enrolledCourses.add(course);
+            System.out.println("Enrolled in course: " + course.getCourseName());
+        } else {
+            System.out.println("Cannot enroll in null course.");
+        }
     }
+
+    
+    
+    public boolean dropCourse(Course course) {
+        if (course != null && enrolledCourses.contains(course)) {
+            enrolledCourses.remove(course);
+            System.out.println("Dropped course: " + course.getCourseName());
+            return true;
+        } else {
+            System.out.println("Course not found or cannot drop a null course.");
+            return false;
+        }
+    }
+
+    
     
     public String viewMarks() {
-        //TODO
-        return "";
+        StringBuilder marksInfo = new StringBuilder("Marks:\n");
+
+        for (Course course : enrolledCourses) {
+            marksInfo.append("Course: ").append(course.getCourseName()).append("\n");
+
+            // Assuming the GradeBook class has a method to retrieve marks
+            GradeBook gradeBook = course.getGradebook();
+            if (gradeBook != null) {
+                // Assuming GradeBook has a method to get marks for the student
+                Vector<Double> studentMarks = gradeBook.getStudentMarks(this);
+
+                if (studentMarks != null && !studentMarks.isEmpty()) {
+                    marksInfo.append("Marks: ").append(studentMarks).append("\n");
+                } else {
+                    marksInfo.append("No marks available for this course.\n");
+                }
+            } else {
+                marksInfo.append("No GradeBook available for this course.\n");
+            }
+        }
+
+        return marksInfo.toString();
     }
+
+    
     
     public String viewTranscript() {
-        //TODO
-        return "";
+        StringBuilder transcriptInfo = new StringBuilder("Transcript:\n");
+
+        for (Course course : enrolledCourses) {
+            transcriptInfo.append("Course: ").append(course.getCourseName()).append("\n");
+
+            // Assuming the GradeBook class has a method to retrieve marks
+            GradeBook gradeBook = course.getGradebook();
+            if (gradeBook != null) {
+                // Assuming GradeBook has a method to get marks for the student
+                Vector<Double> studentMarks = gradeBook.getStudentMarks(this);
+
+                if (studentMarks != null && !studentMarks.isEmpty()) {
+                    transcriptInfo.append("Marks: ").append(studentMarks).append("\n");
+                } else {
+                    transcriptInfo.append("No marks available for this course.\n");
+                }
+            } else {
+                transcriptInfo.append("No GradeBook available for this course.\n");
+            }
+        }
+
+        return transcriptInfo.toString();
     }
+
+    
+    
     
     public String getTranscript() {
-        //TODO
-        return "";
+        StringBuilder transcriptInfo = new StringBuilder("Transcript:\n");
+
+        for (Course course : enrolledCourses) {
+            transcriptInfo.append("Course: ").append(course.getCourseName()).append("\n");
+
+            // Assuming the GradeBook class has a method to retrieve marks
+            GradeBook gradeBook = course.getGradebook();
+            if (gradeBook != null) {
+                // Assuming GradeBook has a method to get marks for the student
+                Vector<Double> studentMarks = gradeBook.getStudentMarks(this);
+
+                if (studentMarks != null && !studentMarks.isEmpty()) {
+                    transcriptInfo.append("Marks: ").append(studentMarks).append("\n");
+                } else {
+                    transcriptInfo.append("No marks available for this course.\n");
+                }
+            } else {
+                transcriptInfo.append("No GradeBook available for this course.\n");
+            }
+        }
+
+        return transcriptInfo.toString();
     }
+
+    
+    
     
     public String getInfoAboutTeacher() {
-        //TODO
-        return "";
+        StringBuilder teacherInfo = new StringBuilder("Teachers Information:\n");
+
+        for (Course course : enrolledCourses) {
+            teacherInfo.append("Course: ").append(course.getCourseName()).append("\n");
+
+            // Assuming the Course class has a method to retrieve the teacher
+            Teacher teacher = course.getTeacher();
+            if (teacher != null) {
+                teacherInfo.append("Teacher: ").append(teacher.getName()).append(" ").append(teacher.getSurname()).append("\n");
+                // Add more teacher information as needed
+            } else {
+                teacherInfo.append("No teacher information available for this course.\n");
+            }
+        }
+
+        return teacherInfo.toString();
     }
+
     
-    public void rateTeachers() {
-        //TODO
-    }
     
-    public void becomeAMemberOfOrganization() {
-        //TODO
+    
+    public void rateTeachers(String selectedCourseName, int rating) {
+        Course selectedCourse = findCourseByName(selectedCourseName);
+
+        if (selectedCourse != null) {
+            Teacher teacher = selectedCourse.getTeacher();
+
+            if (teacher != null) {
+                // You may want to add validation for the rating (e.g., between 1 and 5)
+                teacher.addRating(rating);
+                System.out.println("Teacher rated successfully!");
+            } else {
+                System.out.println("No teacher assigned to the selected course.");
+            }
+        } else {
+            System.out.println("Course not found.");
+        }
     }
+
+    // Helper method to find a course by name
+    private Course findCourseByName(String courseName) {
+        for (Course course : enrolledCourses) {
+            if (course.getCourseName().equalsIgnoreCase(courseName)) {
+                return course;
+            }
+        }
+        return null;
+    }
+
+    
+    
+    
+    
+    public void becomeAMemberOfOrganization(Organization organization) {
+        if (organization != null) {
+            this.organization = organization;
+            System.out.println("Successfully joined the organization: " + organization.getOrganizationName());
+        } else {
+            System.out.println("Cannot join a null organization.");
+        }
+    }
+
     
     
     public String quitOrganization() {
-        //TODO
-        return "";
+        if (organization != null) {
+            String organizationName = organization.getOrganizationName();
+            this.organization = null;
+            return "Successfully quit the organization: " + organizationName;
+        } else {
+            return "Not a member of any organization.";
+        }
     }
     
     
     public static Student createStudent(String name, String surname, Gender gender, String email) {
-    	return new Student(name, surname, gender, email);
+    	Student student = new Student();
+    	student.setName(name);
+    	student.setSurname(surname);
+        student.setGender(gender);
+        student.setEmail(email);
+        return student;
     }
     
 }
