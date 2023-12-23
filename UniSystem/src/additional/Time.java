667227abcd60915209f154491eb263f2861d733c
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-23 22:31:15 +0600

    Update Time.java

--- UniSystem/src/additional/Time.java
+++ UniSystem/src/additional/Time.java
@@ -1,56 +1,77 @@
 package additional;
 
-import Enums.WeekDays;
-import course.Lesson;
+import java.util.Objects;
+
+import course.*;
+
+import enums.*;
+
 
 public class Time {
+    
     private WeekDays day;
     private Integer hour;
     private Integer minutes;
     private Lesson lesson;
-
-    public Time(){
-
-    }
-    public Time(WeekDays day, Integer hour,Integer minutes, Lesson lesson;){
+    
+    public Time(){}
+    public Time(WeekDays day, Integer hour,Integer minutes, Lesson lesson){
         this.day = day;
         this.hour = hour;
         this.minutes = minutes;
         this.lesson = lesson;
     }
-
+    
     public WeekDays getDay() {
         return this.day;
     }
-    public WeekDays setDay(WeekDays day) {
+    
+    public void setDay(WeekDays day) {
         this.day = day;
     }
+    
     public Integer getHour() {
         return this.hour;
     }
-    public Integer setHour(Integer hour) {
+    
+    public void setHour(Integer hour) {
         this.hour = hour;
     }
     public Integer getMinutes() {
         return this.minutes;
     }
-    public Integer setMinutes(Integer minutes) {
+    public void setMinutes(Integer minutes) {
         this.minutes = minutes;
     }
 
-    public Integer getTime(Lesson l){
-        for(Lesson cur:lesson){
-            if(cur.equals(l)){
-                return "Time: "+l.getHour()+":"+l.getMinutes();
-            }
+
+    
+    public String getTime(Lesson l) {
+        if (this.lesson.equals(l)) {
+            return "Time: " + l.getHour() + ":" + l.getMinutes();
         }
+        return null;  // Return something meaningful in case the lesson is not found
     }
 
+    
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Lesson other = (Lesson) obj;
+        return Objects.equals(hour, other.hour) && Objects.equals(minutes, other.minutes);
+    }
 
+    
+    
     public String toString() {
 		return "Day: "+day+"\n"+"Lesson: "+lesson+"\n"+"Time: "+hour+":"+minutes;
 	}
-
 
     
 }
