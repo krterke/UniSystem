1bfa67054bc67ff102402b6e3cf88fd2417eb693
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-23 22:32:16 +0600

    Update LogFile.java

--- UniSystem/src/additional/LogFile.java
+++ UniSystem/src/additional/LogFile.java
@@ -1,36 +1,46 @@
 package additional;
 
+import java.util.Date;
+
+
 import users.Admin;
+import users.User;
+
+
 
 public class LogFile {
-    private Integer userId;
+    
+	private String userId;
     private String action;
     private Admin admin;
-
-    public LogFile(){
-
-    }
-    public LogFile(Integer userId, String action, Admin admin;){
+    
+    public LogFile() {}
+    
+    public LogFile(String userId, String action, Admin admin){
         this.userId = userId;
         this.action = action;
         this.admin = admin;
     }
-
-    private Integer getUserId() {
+    
+    
+    public String getUserId() {
         return this.userId;
     }
-    private Integer setUserId(Integer userId) {
+    
+    public void setUserId(String userId) {
         this.userId = userId;
     }
-    private String getAction() {
+    
+    public String getAction() {
         return this.action;
     }
-    private String setAction(String action) {
+    
+    public void setAction(String action) {
         this.action = action;
     }
-
+    
     public String toString() {
-		return "User: "+userId.getName()+" "+userId.getSurname()+"\n"+"Actions: "+getAction();
+		return "User ID: " + getUserId()+"\n"+"Actions: "+getAction();
 	}
     
 }
