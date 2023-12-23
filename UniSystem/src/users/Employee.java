ee41a5576a2c2abe4d0def4aad78279b74e389ab
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-23 19:57:42 +0600

    Update Employee.java

--- UniSystem/src/users/Employee.java
+++ UniSystem/src/users/Employee.java
@@ -1,54 +1,70 @@
-package Users;
+package users;
 
-import Additional.*;
-import Enums.Gender;
+import java.io.Serializable;
+import additional.*;
+import database.*;
 
-public class Employee extends User implements Serializable{
+import enums.Gender;
+
+public class Employee extends User  implements Serializable {
+	private static final long serialVersionUID = 123456789L;
     
-	private double salary;
+    private double salary;
+    
+	public Employee() {}
 	
-	public Employee() {
-		
+	public Employee(String name, String surname, Gender gender, String email) throws Exception {
+		super(name, surname, gender, email);
 	}
 	
-	public Employee(String name,String surname,String email, int phoneNumber,Gender gender,double salary) {
-		super(name,surname,email,phoneNumber,gender);
-		this.salary=salary;
+    public Employee(String name, String surname, Gender gender, String email, double salary) throws Exception {
+		super(name, surname, gender, email);
+		this.salary = salary;
 	}
-	
-	public Employee(String name,String surname,String email, int phoneNumber) {
-		super(name,surname, email, phoneNumber);
+    public Employee(String name,String surname,String email, int phoneNumber) {
+		super(name,surname, email);
 	}
 	public Employee(String name,String surname) {
 		super(name, surname);
 	}
-
-	public double getSalary() {
+    public Employee(String name, String surname,String log, String pass) {
+		super(name,surname,log,pass);
+	}
+	
+	
+    
+    public double getSalary() {
 		return salary;
 	}
 
 	public void setSalary(double salary) {
 		this.salary = salary;
 	}
-	
-	public void sendMessage(String title,String text,String name,String surname){
+    
+	public void sendMessage(String title,String text,String name,String surname) throws Exception{
 		Employee e=new Employee();
-		for(User cur:Database.users) {
+		for(User cur : Database.getInstance().getUsers()) {
 			if(cur.getName().equals(name) && cur.getSurname().equals(surname)) {
-				e=(Employee)cur;
+				e = (Employee)cur;
 			}
 		}
 		SMS s=new SMS(this,e,title,text);
-		Database.messages.add(s);
+		Database.getInstance().getMessages().add(s);
 	}
 	
-	public String readMessage() {
-		for(SMS s:Database.messages) {
+	
+	public String readMessage() throws Exception {
+		for(SMS s : Database.getInstance().getMessages()) {
 			if(s.getReader().equals(this)) {
 				return s.toString();
 			}
 		}
 		return "You don't have a message";
 	}
-
+    
+    
+    public String toString() {
+        return super.toString() + "Salary: " + salary;
+    }
+    
 }
