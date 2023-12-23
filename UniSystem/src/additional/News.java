eeb8b0b6b9a496dd50f6c8acff85e2af6c727ff8
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-23 22:32:03 +0600

    Update News.java

--- UniSystem/src/additional/News.java
+++ UniSystem/src/additional/News.java
@@ -2,58 +2,68 @@ package additional;
 
 import java.util.Date;
 import enums.*;
-import users.Manager;
+import users.*;
+import database.*;
 
 public class News {
-
     private String title;
-    private String text;
+    public String text;
     private Priority priority;
-    private Date date;;
     private Manager manager;
-    private System system;
-	private Time time;
-	
-    public News(){
+    private Database database;
+    private Date date;
+    
 
+    public News() {}
+    public News(String title, String text) {
+    	this.title = title;
+    	this.text = text;
     }
-    public News(String title, String text, Priority priority, Time time, Manager manager, System system){
+    public News(String title, String text, Priority priority, Date date, Manager manager, Database database){
         this.title = title;
         this.text = text;
         this.priority = priority;
-        this.time = time;
+        this.date = date;  
         this.manager = manager;
-        this.system = system;
+        this.database = database;
     }
-
-    private String getTitle() {
+    
+    
+    
+    public String getTitle() {
         return this.title;
     }
-    private void setTitle(String title) {
+    
+    public void setTitle(String title) {
         this.title = title;
     }
-    private String getText() {
+    
+    public String getText() {
         return this.text;
     }
-    private void setText(String text) {
+    
+    public void setText(String text) {
         this.text = text;
     }
-    private Priority getPriority() {
+    
+    public Priority getPriority() {
         return this.priority;
     }
-    private void setPriority(Priority priority) {
+    
+    public void setPriority(Priority priority) {
         this.priority = priority;
     }
-    private Date getTime() {
+    
+    public Date getTime() {
         return this.date;
     }
-    private Date setTime(Date time) {
-        this.time = time;
-    }
 
+    public void setTime(Date date) {
+        this.date = date;
+    }
+    
     public String toString(){
         return "----------"+"\n"+priority+"\n"+title+"\n"+text+"\n"+date+"\n"+"----------";
     }
-
     
 }
