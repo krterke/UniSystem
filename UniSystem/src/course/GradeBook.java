635f130e5f42741813287dca09dbca4d212c5208
madinasaimova <123065393+madinasaimova@users.noreply.github.com>
2023-12-23 22:29:40 +0600

    Update GradeBook.java

--- UniSystem/src/course/GradeBook.java
+++ UniSystem/src/course/GradeBook.java
@@ -1,78 +1,87 @@
 package course;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Vector;
 
 import enums.*;
+import users.*;
 
 public class GradeBook {
-    private String studentId;
-    private Map<String, Integer> gradeAtt1;
-    private Map<String, Integer> gradeAtt2;
-    private Map<String, Integer> finalExam;
-//    private Map<String, Integer> marks;
-    private Marks mark;
-    private Course course;
     
-    //getters and setters
-    private String getStudentId() {
+   
+    private Integer studentId;
+    private Integer gradeAtt1;
+    private Integer gradeAtt2;
+    private Integer finalExam;
+    private Marks mark;
+//    private Course course;
+    
+   
+    
+    
+    public Integer getStudentId() {
         return this.studentId;
     }
-    private void setStudentId(String studentId) {
+    
+    public void setStudentId(Integer studentId) {
         this.studentId = studentId;
     }
-    private  Map<String, Integer> getGradeAtt1() {
+    
+    public Integer getGradeAtt1() {
         return this.gradeAtt1;
     }
-    private void setGradeAtt1(Map<String, Integer> gradeAtt1) {
+    
+    public void setGradeAtt1(Integer gradeAtt1) {
         this.gradeAtt1 = gradeAtt1;
     }
-    private Map<String, Integer> getGradeAtt2() {
+    
+    public Integer getGradeAtt2() {
         return this.gradeAtt2;
     }
-    private void setGradeAtt2(Map<String, Integer> gradeAtt2) {
+    
+    public void setGradeAtt2(Integer gradeAtt2) {
         this.gradeAtt2 = gradeAtt2;
     }
-    private Map<String, Integer> getFinalExam() {
+    
+    public Integer getFinalExam() {
         return this.finalExam;
     }
-    private void setFinalExam(Map<String, Integer> finalExam) {
+    
+    public void setFinalExam(Integer finalExam) {
         this.finalExam = finalExam;
     }
-    private Marks getMark() {
+    
+    public Marks getMark() {
         return this.mark;
     }
-    private void setMark(Marks mark) {
+    
+    public void setMark(Marks mark) {
         this.mark = mark;
     }
-    
-    public Course getCourse() {
-        return this.course;
-    }
-    
-    public void setCourse(Course course) {
-        this.course = course;
-    }
-    
-    //methods
-    public void viewStudentMarks(String studentId) {
-        if (mark.containsKey(studentId)) {
-            System.out.println("Marks for Student ID " + studentId + ": " + mark.get(studentId));
-        } else {
-            System.out.println("No marks available for Student ID " + studentId);
-        }
-    }
-    
-    public void setStudentMarks(String studentId, int marksValue) {
-        if (mark.contains(studentId)) {
-            mark.replace(studentId, marksValue);
-            System.out.println("Marks updated for Student ID " + studentId);
-        } else {
-            System.out.println("Student ID " + studentId + " not found in the gradebook.");
-        }
-    }
-    
-    
 
+    
+    
+    
+    
+    // Assuming marks are stored in a map with the student as the key
+    private Map<Student, Vector<Double>> marksMap;
+    
+    public GradeBook() {
+    	this.marksMap = new HashMap<>();
+    }
+    
+    // Method to add marks for a student
+    public void addMarks(Student student, Vector<Double> marks) {
+        marksMap.put(student, marks);
+    }
+
+    // Method to get marks for a student
+    public Vector<Double> getStudentMarks(Student student) {
+        return marksMap.get(student);
+    }
+    
+    
     
     
 }
